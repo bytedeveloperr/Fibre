9c0dd05171ce269d35b7c9a354ddcd6de52e6f82
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2022-10-09 02:44:41 +0100

    add internal functions

--- contracts/core/src/account.rs
+++ contracts/core/src/account.rs
@@ -21,11 +21,23 @@ impl Account {
     }
 }
 
-#[near_bindgen]
 impl Fibre {
-    pub fn get_account(&self, account_id: AccountId) -> Account {
+    pub fn internal_save_account(&mut self, account_id: AccountId, account: Account) {
+        assert!(env::is_valid_account_id(account_id.as_bytes()));
+
+        self.accounts.insert(&account_id, &account);
+    }
+
+    pub fn internal_get_account(&self, account_id: AccountId) -> Account {
         self.accounts
             .get(&account_id)
             .unwrap_or(Account::new(account_id))
+    }
+}
+
+#[near_bindgen]
+impl Fibre {
+    pub fn get_account(&self, account_id: AccountId) -> Account {
+        self.internal_get_account(account_id)
     }
 }
